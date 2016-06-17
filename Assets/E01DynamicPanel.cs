900c56cb73fa2f920a6af764f446b8f6e9508be3
Pablo Riedemann <zoserx@gmail.com>
2016-06-16 20:38:11 -0400

    Initial Commit

---
+++ Assets/E01DynamicPanel.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class E01DynamicPanel : MonoBehaviour
+{
+    [SerializeField]
+    private Text _title;
+
+    [SerializeField]
+    private Image _image;
+
+    [SerializeField]
+    private Button _button;
+
+    private E01Window _parentWindow = null;
+
+    public void SetData(E01Window parentWindow,string title)
+    {
+        _title.text   = title;
+        _parentWindow = parentWindow;
+        _button.onClick.AddListener(OnButtonClicked);
+    }
+
+    private void OnButtonClicked()
+    {
+        if(_parentWindow != null)
+        {
+            _parentWindow.SetMainText("Last Pressed "+_title.text);
+        }
+        Debug.LogWarning("Button Clicked");
+    }
+
+}
---
+++ Assets/E01Window.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections.Generic;
+using System.Collections;
+
+public class E01Window : MonoBehaviour
+{
+    [SerializeField]
+    private Button _controlButtonAdd;
+
+    [SerializeField]
+    private Button _controlButtonRemove;
+
+    [SerializeField]
+    private Transform _dynamicPanelParent;
+
+    [SerializeField]
+    private GameObject _referenceDynamicPanel;
+
+    [SerializeField]
+    private Text _mainText;
+
+    private List<E01DynamicPanel> _panelList = new List<E01DynamicPanel>();
+
+    // Use this for initialization
+    void Start()
+    {
+        _controlButtonAdd.onClick.AddListener(AddDynamicPanel);
+        _controlButtonRemove.onClick.AddListener(RemoveDynamicPanel);
+
+        _referenceDynamicPanel.gameObject.SetActive(false);
+    }
+	
+    public void AddDynamicPanel()
+    {
+        E01DynamicPanel panel = CreateDynamicPanel("Panel: "+Random.Range(0,100));
+
+        _panelList.Add(panel);
+    }
+
+    public void RemoveDynamicPanel()
+    {
+        if(_panelList.Count > 0)
+        {
+            E01DynamicPanel panel = _panelList[_panelList.Count - 1];
+            _panelList.RemoveAt(_panelList.Count - 1);
+            Destroy(panel.gameObject);
+        }
+    }
+
+    private E01DynamicPanel CreateDynamicPanel(string title)
+    {
+        GameObject newDynamicPanel = GameObject.Instantiate(_referenceDynamicPanel);
+
+        E01DynamicPanel dynamicPanel = newDynamicPanel.GetComponent<E01DynamicPanel>();
+
+        dynamicPanel.SetData(this,title);
+        dynamicPanel.transform.SetParent(_dynamicPanelParent);
+        dynamicPanel.transform.localScale = Vector3.one;
+        dynamicPanel.transform.localPosition = Vector3.zero;
+        dynamicPanel.gameObject.SetActive(true);
+
+
+        return dynamicPanel;
+    }
+
+    public void SetMainText(string text)
+    {
+        _mainText.text = text;
+    }
+    
+}
